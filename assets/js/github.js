0f4152e7611bac81bc3f98554900e8d75a621d98
Fayzaan Ali <57409767+fayzaanali@users.noreply.github.com>
2024-09-13 19:03:37 +0100

    updated website
    updated css and github js

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,7 +1,9 @@
 var tags = {
   "fayzaanali.github.io": [" HTML", " CSS", " JS", " Website"],
   "Pacman-S2D": [" C++", " Game-Engine", " OpenGL"],
-  "Mario-SDL": [" C++", " Game-Engine", " OpenGL", " SDL"]
+  "Mario-SDL": [" C++", " Game-Engine", " OpenGL", " SDL"],
+  "FYP" : [" Unity", " UI", " C#", " PC Game"],
+  "Tile-Mobile-Game" : [" Unity", " UI", " C#", " Mobile Game"]
  }
 
 function fetchGithub(githubUser){  
@@ -9,18 +11,19 @@ function fetchGithub(githubUser){
     fetch('https://api.github.com/users/' + githubUser).then(res => res.json()).then(data => {
       fetch('https://api.github.com/users/' + githubUser + '/repos').then(res => res.json()).then(repo => {
       var mainContainer = document.getElementById("row");
+      
         for (i = 0; i < data.public_repos; i++) {
           if (tags[repo[i].name] == undefined) tags[repo[i].name] = "N/A";
           if (repo[i].language == undefined) repo[i].language = "N/A";
           var div = document.createElement("div");
           div.innerHTML = 
-          `<div class="col s12 m6">
+          `<div class="col s12 m4">
             <a href="${repo[i].html_url}" target="_blank">
               <div class="card darken-1">
                 <div class="card-content white-text">
-                  <span class="card-title">${repo[i].name}</span>
+                  <span class="card-title">${repo[i].name} <i class="fas fa-external-link-alt title-link"></i></span>
                   <p class="card-description">${repo[i].description}</p>
-                  <p class="small-text">${tags[repo[i].name]}</p>
+                  <p class="small-text tags">${tags[repo[i].name]}</p>
                 </div>
                 <div class="card-action">
                   <span class="code-colour" style="background-color: ${colour[repo[i].language] || "#fff"};"></span> ${repo[i].language}
