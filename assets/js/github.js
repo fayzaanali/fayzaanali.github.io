67b6b1457fd7adbdb1b51533e4cbb236675c9af0
Fayzaan Ali <57409767+fayzaanali@users.noreply.github.com>
2024-09-15 05:10:25 +0100

    updated github.js

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,19 +1,10 @@
-var tags = {
-  "fayzaanali.github.io": [" HTML", " CSS", " JS", " Website"],
-  "Pacman-S2D": [" C++", " Game-Engine", " OpenGL"],
-  "Mario-SDL": [" C++", " Game-Engine", " OpenGL", " SDL"],
-  "FYP" : [" Unity", " UI", " C#", " PC Game"],
-  "Tile-Mobile-Game" : [" Unity", " UI", " C#", " Mobile Game"]
- }
-
 function fetchGithub(githubUser){  
   fetch('assets/js/json/colour.json').then(res => res.json()).then(colour => {
     fetch('https://api.github.com/users/' + githubUser).then(res => res.json()).then(data => {
       fetch('https://api.github.com/users/' + githubUser + '/repos').then(res => res.json()).then(repo => {
       var mainContainer = document.getElementById("row");
-      
         for (i = 0; i < data.public_repos; i++) {
-          if (tags[repo[i].name] == undefined) tags[repo[i].name] = "N/A";
+          if (repo[i].topics == undefined) repo[i].topics = "N/A";
           if (repo[i].language == undefined) repo[i].language = "N/A";
           var div = document.createElement("div");
           div.innerHTML = 
@@ -23,11 +14,11 @@ function fetchGithub(githubUser){
                 <div class="card-content white-text">
                   <span class="card-title">${repo[i].name} <i class="fas fa-external-link-alt title-link"></i></span>
                   <p class="card-description">${repo[i].description}</p>
-                  <p class="small-text tags">${tags[repo[i].name]}</p>
+                  <p class="small-text tags">${repo[i].topics.toString().split(',').join(', ').toUpperCase()} </p>
                 </div>
                 <div class="card-action">
                   <span class="code-colour" style="background-color: ${colour[repo[i].language] || "#fff"};"></span> ${repo[i].language}
-                  <div style="float: right; margin-right: 0;"><i class="fas fa-code-branch"></i> ${repo[i].forks_count} <i class="fas fa-star"></i> ${repo[i].stargazers_count}</div>
+                  <div style="float: right; margin-right: 0;"><i class="fas fa-code-branch"></i> ${repo[i].forks_count} <i class="fas fa-star"></i> ${repo[i].stargazers_count} <i class="far fa-calendar" style="padding-left: 10px; padding-right: 2px"></i> ${new Date(repo[i].created_at).getUTCFullYear()}</div>
                 </div>
               </div>
             </a>
@@ -38,6 +29,4 @@ function fetchGithub(githubUser){
     }).catch(err => console.error(err));
   })
 };
-
 fetchGithub("fayzaanali");
-
